7bd5e4287dfd5062fd4f646347dbbcafb5be4867
Rafael Contreras <rafael.contreras@cashconverters.co.nz>
2018-09-19 12:38:14 +1200

    Vector drawable

--- android/src/main/java/com/rnfingerprint/FingerprintDialog.java
+++ android/src/main/java/com/rnfingerprint/FingerprintDialog.java
@@ -36,7 +36,7 @@ public class FingerprintDialog extends DialogFragment implements FingerprintHand
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setStyle(DialogFragment.STYLE_NORMAL, android.R.style.Theme_Material_Light_Dialog);
+        setStyle(DialogFragment.STYLE_NORMAL, R.style.TouchIdDialog);
         setCancelable(false);
     }
 
